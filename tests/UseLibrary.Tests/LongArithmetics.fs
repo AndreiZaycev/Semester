0ab7cbde6b66d9ba56f0c7d117f667e702b8afb1
Andrew <Mikowftv3@mail.ru>
2021-06-03 12:11:12 +0300

    fixes file cause of wrong merge

--- tests/UseLibrary.Tests/LongArithmetics.fs
+++ tests/UseLibrary.Tests/LongArithmetics.fs
@@ -1,9 +1,6 @@
 module LongArithmetic
-
 open Expecto
-
 open BigAriphmetics
-
 open Listik
 
 let template valuesTest operator (operator1: BigInt -> BigInt -> BigInt) name =
@@ -17,8 +14,8 @@ let template valuesTest operator (operator1: BigInt -> BigInt -> BigInt) name =
                let secondInt = fold (fun acc elem -> string elem + acc) "" (rev second.digits) |> int
                if secondInt <> 0 then
                    Expect.equal
-                       (operator (firstInt * first.sign) (secondInt * second.sign))
-                       ((operator1 first second).sign * (fold (fun acc elem -> string elem + acc) "" (rev (operator1 first second).digits) |> int))
+                       (operator (firstInt * first.Sign) (secondInt * second.Sign))
+                       ((operator1 first second).Sign * (fold (fun acc elem -> string elem + acc) "" (rev (operator1 first second).digits) |> int))
                        "equals"
 
 [<Tests>]
